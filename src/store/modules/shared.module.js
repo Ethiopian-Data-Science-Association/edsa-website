4ead4b2b9606e04752e506bf25460d92fdb94f19
Brook Tarekegn <brooksideas@gmail.com>
2024-11-23 03:52:45 -0800

    code: Shared module for document Storage/File Upload implementation

--- src/store/modules/shared.module.js
+++ src/store/modules/shared.module.js
@@ -1,79 +1,116 @@
-import { getField, updateField } from "vuex-map-fields";
-import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
+import { getField, updateField } from 'vuex-map-fields'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject
+} from 'firebase/storage'
 
 const state = {
-  documentPath: "",
+  documentPath: '',
   uploadingDocument: false,
-  deletingDocument: false,
-};
+  deletingDocument: false
+}
 
 const actions = {
   async uploadDocument({ commit }, { payload }) {
-    commit("updateField", { path: "uploadingDocument", value: true });
-
+    commit('updateField', { path: 'uploadingDocument', value: true })
     try {
-      const storage = getStorage(); // Initialize Firebase storage
-      const storageRef = ref(storage, `/uploads/${encodeURIComponent(payload.documentUrl)}/${encodeURIComponent(payload.documentName)}`);
+      const storage = getStorage() // Initialize Firebase storage
+      const storageRef = ref(storage, `/uploads/${payload.documentStoragePath}`)
+      const uploadTask = uploadBytesResumable(storageRef, payload.document, payload.metadata)
 
-      // Upload the document
-      await uploadBytes(storageRef, payload.document, { contentType: payload.metadata });
-
-      // Retrieve the document's download URL
-      const documentUrl = await getDownloadURL(storageRef);
-
-      // Update state with the document URL
-      commit("updateField", { path: "documentPath", value: documentUrl });
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          // 'Upload is ' + progress + '% done'
+          switch (snapshot.state) {
+            case 'paused':
+              // 'Upload is paused'
+              break
+            case 'running':
+              // 'Upload is running'
+              break
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break
+            case 'storage/canceled':
+              // User canceled the upload
+              break
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            // Update state with the document URL
+            commit('updateField', { path: 'documentPath', value: downloadURL })
+          })
+        }
+      )
     } catch (error) {
-      console.error("Error uploading document:", error);
+      console.error('Error uploading document:', error)
 
       // Clear document path on error
-      commit("updateField", { path: "documentPath", value: "" });
+      commit('updateField', { path: 'documentPath', value: '' })
     } finally {
-      commit("updateField", { path: "uploadingDocument", value: false });
+      commit('updateField', { path: 'uploadingDocument', value: false })
     }
   },
 
   async removeDocument({ commit }, { payload }) {
-    commit("updateField", { path: "deletingDocument", value: true });
+    commit('updateField', { path: 'deletingDocument', value: true })
 
     try {
-      const storage = getStorage(); // Initialize Firebase storage
-      const storageRef = ref(storage, `/uploads/${encodeURIComponent(payload.documentUrl)}/${encodeURIComponent(payload.documentName)}`);
+      const storage = getStorage() // Initialize Firebase storage
+      const storageRef = ref(storage, `/uploads/${payload.documentUrl}/${payload.documentName}`)
 
       // Delete the document
-      await deleteObject(storageRef);
+      await deleteObject(storageRef)
 
       // Clear the document path
-      commit("updateField", { path: "documentPath", value: "" });
+      commit('updateField', { path: 'documentPath', value: '' })
     } catch (error) {
-      console.error("Error deleting document:", error);
+      console.error('Error deleting document:', error)
 
       // Optionally: Notify user of failed deletion
     } finally {
-      commit("updateField", { path: "deletingDocument", value: false });
+      commit('updateField', { path: 'deletingDocument', value: false })
     }
   },
 
   clearDocumentPath({ commit }) {
-    commit("updateField", { path: "documentPath", value: "" });
-  },
-};
+    commit('updateField', { path: 'documentPath', value: '' })
+  }
+}
 
 const mutations = {
-  updateField,
-};
+  updateField
+}
 
 const getters = {
   getField,
   uploadingDocument: (state) => state.uploadingDocument,
   deletingDocument: (state) => state.deletingDocument,
-  documentPath: (state) => state.documentPath,
-};
+  documentPath: (state) => state.documentPath
+}
 
 export default {
   namespaced: true,
   state,
   actions,
   mutations,
-  getters,
-};
+  getters
+}
