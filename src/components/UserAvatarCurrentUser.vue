490f0d27127064a0c35937f8d657911150a80db2
Liben Hailu <libenhailu04@gmail.com>
2025-01-01 16:48:31 +0300

    fix/profile-user-card-integration

--- src/components/UserAvatarCurrentUser.vue
+++ src/components/UserAvatarCurrentUser.vue
@@ -1,12 +1,22 @@
 <script setup>
-import { useMainStore } from '@/pinia/main'
 import UserAvatar from '@/components/UserAvatar.vue'
+import { useMainStore } from '@/pinia/main';
 
 const mainStore = useMainStore()
+const props = defineProps({
+  avatarUrl: {
+    type: String,
+    default: '' 
+  },
+  username: {
+    type: String,
+    required: true
+  },
+})
 </script>
 
 <template>
-  <UserAvatar :username="mainStore.userName" :avatar="mainStore.userAvatar">
+  <UserAvatar :username="props.userName" :avatar="props.avatarUrl">
     <slot />
   </UserAvatar>
 </template>
