51ae7845e6daa305734364d66ffcba0836d9258b
Brook Tarekegn <brooksideas@gmail.com>
2025-01-17 13:10:47 -0800

    code: Library store setup

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -3,3 +3,4 @@ export { default as user } from "./user.module";
 export {default as blogs} from "./blogs.module";
 export { default as shared} from "./shared.module";
 export { default as certification } from "./certification.module"; 
+export { default as library } from "./library.module"; 
